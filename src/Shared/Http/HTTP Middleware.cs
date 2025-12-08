1b84b79db0ebe049064616276c036763aebae3c9
Xamarializ <Xamarializ08@gmail.com>
2025-12-08 19:20:01 -0400

    Initial commit

--- src/Shared/Http/HTTP Middleware.cs
+++
@@ -1,6 +0,0 @@
-namespace Shared.Http;
-
-using System.Collections;
-using System.Net;
-
-public delegate Task HttpMiddleware(HttpListenerRequest req, HttpListenerResponse res, Hashtable props, Func<Task> next);
