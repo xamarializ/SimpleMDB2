c11c4851fc1527f58f99569b8e5e19870fb34082
xamarializ <xamarializ08@gmail.com>
2025-12-10 12:18:46 -0400

    Update API_BASE URL to production endpoint

--- src/Smdb.Csr/wwwroot/Scripts/common.js
+++ src/Smdb.Csr/wwwroot/Scripts/common.js
@@ -1,4 +1,4 @@
-export const API_BASE = 'http://localhost:8080/api/v1';
+export const API_BASE = "https://smdb-0791-api-grdwd2g3csckepbz.centralus-01.azurewebsites.net";
 export const $ = (sel, el = document) => el.querySelector(sel);
 export const $$ = (sel, el = document) => Array.from(el.querySelectorAll(sel));
 export const getQueryParam = (k) => new URLSearchParams(location.search).get(k);
